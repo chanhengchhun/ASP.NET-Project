4eb978f0d619fc829c37d9550391b9a1e2f4ef76
Jey Chhun <136158401+chanhengchhun@users.noreply.github.com>
2026-06-28 21:34:00 -0400

    add services, dependancy injection

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
